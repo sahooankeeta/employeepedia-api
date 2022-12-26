fda9f68ab057028519c463eacaebcaf699d26f68
Ankeeta Sahoo <sahooankeeta@gmail.com>
2022-12-26 22:14:12 +0530

    better

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ mongoose.connect(DB).then((con) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cors());
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
