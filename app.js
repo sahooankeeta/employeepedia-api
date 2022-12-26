771bb392dd042e5175ad5b643aa94c3c54521d1d
Ankeeta Sahoo <sahooankeeta@gmail.com>
2022-12-27 00:03:14 +0530

    cors

--- app.js
+++ app.js
@@ -18,17 +18,7 @@ app.use(bodyParser.urlencoded());
 app.use(cors());
 
 app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
